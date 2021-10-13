df7b1e54f258f3e8726debdc133b84ade36abcb4
dataweb1 <bert@data-web.be>
2021-10-13 11:16:50 +0200

    all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     const params = new URLSearchParams(window.location.search)
     if (params.has('all')) {
-        sessionStorage.setItem('all', params["all"]);
+        all = params.get("all");
+        if (all == "") {
+            all = 1;
+        }
+        sessionStorage.setItem('all', all);
     }
-console.log(params);
+
     let all = sessionStorage.getItem('all');
     if (all == 1) {
         $(".published-no").css("display", "block");
