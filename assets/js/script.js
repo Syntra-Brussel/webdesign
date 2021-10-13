f8f27deddd317b039a2483dfa00a1d896d8ac5e0
dataweb1 <bert@data-web.be>
2021-10-13 11:11:47 +0200

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     if (params.has('all')) {
         sessionStorage.setItem('all', params["all"]);
     }
-
+console.log(params);
     let all = sessionStorage.getItem('all');
     if (all == 1) {
         $(".published-no").css("display", "block");
